da55cf43aefcad9480a4b068784bccbdefde5fd4
Steve Taylor <steve.taylor@openmakesoftware.com>
2019-02-19 09:34:06 -0700

    Set user to null on logout

--- src/_store/account.module.js
+++ src/_store/account.module.js
@@ -28,6 +28,8 @@ const actions = {
     },
     logout({ commit }) {
         userService.logout();
+        state = { status: {}, user: null, bikeframe: null, bikefront: null, bikerear: null, bikeseat: null };
+        user = null;
         commit('logout');
     },
     register({ dispatch, commit }, user) {
